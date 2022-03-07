bef854f7b3930108339bd6a7c116f23e36f4be17
joe madamba <josephm.madamba@gmail.com>
2022-03-06 22:19:39 -0500

    normalize text case for text describing what the password is for

--- passGenerator.py
+++ passGenerator.py
@@ -7,8 +7,7 @@ from os import path
 
 print('Create random password')
 
-pass_length = int(
-    input('what is the minimum length required for your password?: '))
+pass_length = int(input('what is the minimum length required for your password?: '))
 
 passwordIsUsedFor = str(input('What is this password for?:'))
 
@@ -24,7 +23,7 @@ passFileExists = path.exists("passFile.txt")
 
 if passFileExists:
     password_file = open("passFile.txt", "a")
-    password_file.write(passwordIsUsedFor + ": " + password + "\n")
+    password_file.write(passwordIsUsedFor.upper() + ": " + password + "\n")
     password_file.close()
 else:
     password_file = open("passFile.txt", "w")
